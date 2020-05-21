7103e4651ee3a8fe01108ac6c1a5bcb6d7d87532
yukidaminori <rinngo903@gmail.com>
2020-05-21 06:30:33 +0000

    follow, unfollow

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,26 +3,16 @@
 @section('content')
     <div class="row">
         <aside class="col-sm-4">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">{{ $user->name }}</h3>
-                </div>
-                <div class="card-body">
-                    <img class="rounded img-fluid" src="{{ Gravatar::src($user->email, 500) }}" alt="">
-                </div>
-            </div>
+            @include('users.card', ['user' => $user])
         </aside>
         <div class="col-sm-8">
-             <ul class="nav nav-tabs nav-justified mb-3">
-                <li class="nav-item"><a href="{{ route('users.show', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/' . $user->id) ? 'active' : '' }}">Timeline <span class="badge badge-secondary">{{ $count_posts }}</span></a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
-            </ul>
+            @include('users.navtabs', ['user' => $user])
             @if (Auth::id() == $user->id)
                 {!! Form::open(['route' => 'posts.store']) !!}
                     <div class="form-group">
+                        {!! Form::textarea('title', old('title'), ['class' => 'form-control', 'rows' => '1']) !!}
                         {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
-                        {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
+                        {!! Form::submit('投稿', ['class' => 'btn btn-primary btn-block']) !!}
                     </div>
                 {!! Form::close() !!}
             @endif
